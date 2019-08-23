5149c152663c1c292708f70480dbf95ae9976b70
freees <468027411@qq.com>
2019-08-23 09:08:59 +0800

    修改启动路径

--- start.php
+++ start.php
@@ -29,9 +29,12 @@ define('GLOBAL_START', 1);
 require_once __DIR__ . '/vendor/autoload.php';
 
 // 加载所有Applications/*/start.php，以便启动所有服务
-foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
-{
-    require_once $start_file;
-}
+//foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
+//{
+//    require_once $start_file;
+//}
+require_once __DIR__.'/app/ChatServer/start_businessworker.php';
+require_once __DIR__.'/app/ChatServer/start_gateway.php';
+require_once __DIR__.'/app/ChatServer/start_register.php';
 // 运行所有服务
 Worker::runAll();
